6af6c69fe4951acea933b67e01470cff0fbb3ae8
deseram07 <w.deseram@uqconnect.edu.au>
2013-08-16 17:28:04 +1000

    Seperated files

--- Tute1Pussel.py
+++ Tute1Pussel.py
@@ -20,25 +20,47 @@ def actionspace(Coordinates):
     elif column == 2:
 #        cannot go right
         possible.remove((0,1))
-#    return possible
-    print possible
+    return possible
     
         
-def sucessor(current_state, actionspace):
+def sucessor(previous_state_matrix):
     '''
-    find the position of the empty time (0 coordinate) in the 
+    find the position of the empty slot (0 coordinate) in the 
     current state matrix and apply the actionspace. Function
     returns the world dynamics 
     '''
+    current_state = [] 
+    previous_state_shape = previous_state_matrix.shape
+    req_coord = [0,0]
+    for item in previous_state_matrix:
+        if req_coord[1] == previous_state_shape[0]-1:
+            req_coord[0] += 1
+            req_coord[1] = 0
+        if item == 0:
+            break
+        req_coord[1] += 1
     
+    possible = actionspace(req_coord)
     
+    for coordinate in possible:
+        current_state_matrix = np.zeros(previous_state_matrix.shape)
+        current_state_matrix = np.copy(previous_state_matrix)
+        r,c = coordinate
+        to_r, to_c = (req_coord[0]+r, req_coord[1]+c)  #other cell affected due to change
+        from_r, from_c = req_coord
+        current = [previous_state_matrix[to_r,to_c], previous_state_matrix[from_r, from_c]]
+        current_state_matrix[to_r,to_c] = current[1]
+        current_state_matrix[from_r, from_c] = current[0]
+        current_state.append(current_state_matrix)
+        
+    print current_state    
 
 def main(input_filename):
     input_file = open(input_filename, 'r')
     var = input_file.readlines()
     startmatrix = np.array(var[0].strip('\n').strip(' ').split(' ')).reshape(3,3)
     finishmatrix = np.array(var[1].strip('\n').strip(' ').split(' ')).reshape(3,3)
-    
+    sucessor(startmatrix)
     
     
 if __name__ == '__main__':
--- assignment1/assg1_42027249_42229904.py
+++ assignment1/assg1_42027249_42229904.py
@@ -1,47 +1,7 @@
 import sys
 import numpy as np
 import math
-
-
-# working on grid (1000 x 1000) where obstacles are represented by 1s and clear is represented by 0s
-
-# creating an ASV
-class ASV:
-    def __init__(self, num, x, y):
-        self.num = num
-        self.x = x
-        self.y = y
-        self.previous = None
-        self.boom = 0
-
-
-# for printing the list of ASVs    
-def print_asv(asv):
-    for i in range(len(asv)):
-        print str(asv[i].num)+', '+str(asv[i].x)+', '+str(asv[i].y)+', '+str(asv[i].previous)
-
-
-# takes a list of string decimals and converts to a number and returns list of ints
-def remove_decimal(list):
-    for i in range(len(list)):
-        list[i] = int(float(list[i])*1000)
-        
-    return list
-
-#calculates the distance between two points
-def boom_length(start, end):
-    boom = math.sqrt((start.x-end.x)**2+(start.y-end.y)**2)
-    return boom
-
-
-# calculates angle between 3 ASVs
-def angles(start, middle, end):
-    a = boom_length(middle, end)
-    b = boom_length(start, middle)
-    c = boom_length(start, end)
-    angle = math.degrees(math.acos((a**2+b**2-c**2)/(2*a*b)))
-    print angle
-
+from utility import *
 
 def main(filename):
     global minArea
