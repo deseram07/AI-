3e3d9f596ee2c84b7851176d76798e8a9e14159d
deseram07 <w.deseram@uqconnect.edu.au>
2013-08-18 16:23:43 +1000

    almost done

--- assignment1/assg1_42027249_42229904.py
+++ assignment1/assg1_42027249_42229904.py
@@ -3,24 +3,26 @@ import numpy as np
 import math
 from utility import *
 import heapq
+import cv2
 
 # Working on grid (1000 x 1000) where obstacles are represented by 1s and clear is represented by 0s
 
 def init_grid(grid):
     for x in range(AStar.width):
         for y in range(AStar.height):
-            if grid[x][y] == 1:
+            if grid[y][x] == 1:
                 reachable = False
             else:
                 reachable = True
             AStar.cells.append(Cell(x, y, reachable))
             
-def display_path():
+def display_path(outputfile):
     print 'path found'
     cell = AStar.end
     while cell.parent is not AStar.start:
         cell = cell.parent
-        print '%d, %d' % (cell.x, cell.y)
+        outputfile.write('%.3f, %.3f\n' % (float(cell.x)/10000, float(cell.y)/1000.0))
+#        print '%d, %d' % (cell.x, cell.y)
         
 # returns the estimated cost to destination from current position
 def get_h(cell):
@@ -59,8 +61,7 @@ def process(asv):
         f, cell = heapq.heappop(AStar.op)
         AStar.cl.add(cell)
         if cell is AStar.end:
-            display_path()
-            break
+            return cell
         adj_cells = get_adj(cell)
         for c in adj_cells:
             if c.reachable and c not in AStar.cl:
@@ -79,6 +80,7 @@ def main(inputfile, outputfile):
     AStar = Astar()
     grid = np.zeros(shape=(1000, 1000))
     file = open(inputfile, 'r')
+    output = open(outputfile, 'w')
     lines = file.readlines()
     start = remove_decimal(lines[1].strip('\n').split(' '))
     finish = remove_decimal(lines[2].strip('\n').split(' '))
@@ -94,14 +96,29 @@ def main(inputfile, outputfile):
 
     # Extracts how much the obstacles are required to be expanded by 
     expand = obstacle_transform(asv)
+    
+#    src = cv2.cv.fromarray(grid)
+#    cv2.cv.ShowImage('win1', src)
+#    cv2.waitKey()
 
     # Creating obstacles in grid
     for j in range(int(lines[3].strip('\n'))):
         obstacle = remove_decimal(lines[j + 4].strip('\n').split(' '))
-        grid[(obstacle[1] + expand[0]):(obstacle[5] + expand[1]+1),(obstacle[0] + expand[2]):(obstacle[4] + expand[3]+1)] = 1
-    
+        final_obstacle = [obstacle[1] + expand[0], obstacle[5] + expand[1] + 1, obstacle[0] + expand[2], obstacle[4] + expand[3] + 1]
+        index = 0
+        while index < len(final_obstacle):
+            if final_obstacle[index] < 0:
+                final_obstacle[index] = 0
+            index += 1 
+        
+        grid[final_obstacle[0]:final_obstacle[1], final_obstacle[2]:final_obstacle[3]] = 1
+
+
     init_grid(grid)
     process(asv)
+    display_path(output)
+    output.close()
+    file.close()
     
         
 
