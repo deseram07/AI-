5db3c2ba810057a9477b8126d6c90c93012d9897
deseram07 <w.deseram@uqconnect.edu.au>
2013-08-20 14:58:00 +1000

    Added line intersection into utilitedit.py

--- assignment1/utilityedit.py
+++ assignment1/utilityedit.py
@@ -105,7 +105,7 @@ def angles(start, middle, end):
 
 def ccw(A,B,C):
     return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
-
+ 
 # Return true if line segments AB and CD intersect
 def intersect(A,B,C,D):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
