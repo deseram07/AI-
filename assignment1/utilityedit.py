b2ac76feb6d064f83e1749fdaff22baad093409a
deseram07 <w.deseram@uqconnect.edu.au>
2013-08-20 15:01:20 +1000

    shits fucked

--- assignment1/utilityedit.py
+++ assignment1/utilityedit.py
@@ -103,11 +103,11 @@ def angles(start, middle, end):
     angle = math.degrees(math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b)))
     return angle
 
-def ccw(A,B,C):
-    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
- 
 # Return true if line segments AB and CD intersect
 def intersect(A,B,C,D):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+def ccw(A,B,C):
+    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
+
 
     
