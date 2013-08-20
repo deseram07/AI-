f0d3f4c86869aa8286f28bcae04cf1f192fdee81
deseram07 <w.deseram@uqconnect.edu.au>
2013-08-20 15:03:59 +1000

    just ignore this shit

--- assignment1/utility.py
+++ assignment1/utility.py
@@ -113,11 +113,10 @@ def area(asv):
     add = add + (asv[number].x * asv[0].y - asv[number].y * asv[0].x)
     return abs(add / 2)
 
-def ccw(A, B, C):
-    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
-
 # Return true if line segments AB and CD intersect
 def intersect(A, B, C, D):
     return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+def ccw(A, B, C):
+    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
 
 
