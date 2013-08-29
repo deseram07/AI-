570c36a1b2c98e1f7c73510c017282e164379cbb
deseram07 <w.deseram@uqconnect.edu.au>
2013-08-29 13:34:47 +1000

    shits working

--- assignment1/utility.py
+++ assignment1/utility.py
@@ -112,7 +112,7 @@ def area(asv):
     for i in range(number - 1):
         add = add + (asv[i].x * asv[i + 1].y - asv[i].y * asv[i + 1].x)
         
-    add = add + (asv[number].x * asv[0].y - asv[number].y * asv[0].x)
+    add = add + (asv[number-1].x * asv[0].y - asv[number-1].y * asv[0].x)
     return abs(add / 2)
 
 # Return true if line segments AB and CD intersect
@@ -131,25 +131,20 @@ def random_length_angle(asv):
     lengths = []
     
     n = len(asv)    #number of asv's
-    r_min = 7*(n-1)
-    min_area = np.pi * r_min**2 # minimum allowed area
-    i = 0
 #    obtaining random lengths for booms
     for i in range(len(asv)-1):
         lengths.append(random.randrange(50,75))
     
-    i = 0    
 #    obtaining random angles between booms
     for i in range(n-1):
         angle_sample.append(random.random())
     angle_sample.append(1.0)
     angle_sample.sort()
-    i = 0
+
     for i in range(len(angle_sample)-1):
         pre_angles.append((angle_sample[i+1] - angle_sample[i])*2.0*np.pi)
-    print angle_sample
+   
     if asv[0].direction < 0:
-        print 'clockwise'
 #        if rotating clockwise
         i = 0
         for i in range(len(pre_angles) - 1):
@@ -172,17 +167,13 @@ def generate_coordinates(lengths, angles, asv):
     init_coord = [0,0]
     coordinate = []
 #    placing the point in random location, this ensures that point lies on gird
-    orig = [random.randrange(1000),random.randrange(1000)]
-
+    shift = [(random.random())*1000.0,(random.random())*1000.0]
     rotate = random.random() * np.pi * 2.0
     
 #    first find the points with initial boom on x axis with 0 degree rotation
     coordinate.append(init_coord)
-#    coordinate.append([init_coord[0] + lengths.pop(0),0])
-#    angles.pop(0)
+
     for i in range(len(lengths)):
-        print lengths
-        print coordinate
         last_point = coordinate[-1]
         [x,y] = last_point
         angle = angles.pop(0)
@@ -190,11 +181,64 @@ def generate_coordinates(lengths, angles, asv):
         x = x + (length * np.cos(angle))
         y = y + (length * np.sin(angle))
         coordinate.append([x,y])
-    print coordinate
+    shift = Shift(coordinate,shift)
     
+    coordinate = Rotate2D(shift, rotate)
+    return check(coordinate,asv)
     
+
+def check(coordinate,asv):
+    for coord,ASV in zip(coordinate, asv):
+        ASV.x = coord[0]
+        ASV.y = coord[1]
+    A = area(asv)
+    n = len(asv)
+    r_min = 7*(n-1)
+    min_area = np.pi * r_min**2 # minimum allowed area
     
+    """
+    Area greater than min_area
+    All points within (1000,1000) grid
+    ASV combination not in obstacle
+    """
+    for i in asv:
+        if 0 <= i.x <= 1000 and  0 <= i.y <= 1000:
+            pass
+        else:
+            return False
+    return A>min_area
+
+def Shift(pts, delta):
+    [dx,dy] = delta
+    for i in range(len(pts)):
+        pts[i][0] += dx
+        pts[i][1] += dy
+    return pts
+
+def Rotate2D(pts, thetha):
+    """
+    Rotates a gives polygon about the origin in the counter clockwise direction 
+    by (thetha) degrees
+    """
+    ang = np.deg2rad(thetha)
+    pts = np.array(pts)
+    cnt = np.array([0,0])
+    return (np.dot(pts-cnt,np.array([[np.cos(ang),np.sin(ang)],[-np.sin(ang),np.cos(ang)]]))+cnt).tolist()
     
+def obtain_random_points(asv, n = 5):
+    points = []
+    sample = []
+    count = 0
+    while count < n:
+        lengths, angles = random_length_angle(asv)
+        if generate_coordinates(lengths, angles, asv):
+            for i in asv:
+                sample.append([i.x,i.y])
+            points.append(sample)
+            sample = []
+            count += 1
+    return points
+
 def constrained_sum_sample_pos(n, total):
     """Return a randomly chosen list of n positive integers summing to total.
     Each such list is equally likely to occur."""
