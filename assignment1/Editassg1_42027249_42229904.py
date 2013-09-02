81685d168e6a933efed411e8193f34e469d93c63
ramykl <r.loveridge@hotmail.com>
2013-09-02 13:24:20 +1000

    edit has extra debug stuff

--- assignment1/Editassg1_42027249_42229904.py
+++ assignment1/Editassg1_42027249_42229904.py
@@ -5,20 +5,7 @@ from utilityedit import *
 import heapq
 # import cv2
 
-# Working on grid (1000 x 1000) where obstacles are represented by 1s and clear is represented by 0s
-
-# def init_grid(grid):
-# #     array = [[[] for x in range(AStar.width / AStar.grid)] for y in range(AStar.width / AStar.grid)]
-# #     x = 0
-# #     y = 0
-#     for x in range(AStar.width / AStar.grid):
-#         for y in range(AStar.width / AStar.grid):
-#             if grid[y][x] == 1:
-#                 reachable = False
-#             else:
-#                 reachable = True
-#             AStar.cells.append(Cell(x, y, reachable))
-
+            
 # gets distance from one sample configuration to another
 def get_dist(sample, other):
     distx = []
@@ -34,14 +21,15 @@ def get_dist(sample, other):
 def get_decimal(coords):
     points = []
     for i in range(len(coords)):
-        points.append(coords[i] / 1000)
+        points.append(float(coords[i]) / 1000.0)
     return points
 
 # output the path to file
-def display_path(outputfile):
+def display_path(outputfile, sample, obstacles):
+
 #     print 'path found'
-    sample = AStar.end
     path = []
+#     print AStar.start.coords
     # creates path from the end to the start inserting into the beginning 
     while sample.parent is not AStar.start:
         coords = get_decimal(sample.coords)
@@ -51,7 +39,30 @@ def display_path(outputfile):
     coords = get_decimal(AStar.start.coords)
     path.insert(0, coords)
     # outputs the path with the coordinates of each configuration separated by spaces
+    xp = []
+    yp = []
+    if debug:
+        oxs = []
+        oys = []
+        for j in obstacles:
+            x = j[::2]
+            y = j[1::2]
+            x.append(j[0])
+            y.append(j[1])
+            oxs.append(x)
+            oys.append(y)
     for x in range(len(path)):
+        if debug:
+            for i in range(len(path[x]) / 2):
+                xp.append(1000.0 * path[x][i * 2])
+                yp.append(1000.0 * path[x][i * 2 + 1])
+            print xp, yp
+            for i in range(len(oxs)):
+                    py.plot(oxs[i], oys[i], '-+')
+            py.plot(xp, yp, '-+')
+            py.show()
+            xp = []
+            yp = []
         outputfile.write(' '.join(map(str, path[x])))
         
 # returns the estimated cost to destination from current position
@@ -61,7 +72,7 @@ def get_h(sample):
     return dist
 
 # returns samples from a given location
-def get_samples(x, y):
+def get_samples(array, x, y):
     return array[y][x]
 
 # updates adjacent sample with details from current sample
@@ -72,7 +83,7 @@ def update_sample(adj, sample):
     adj.f = adj.g + adj.h
     
 # get nearby samples based on centroid position
-def get_adj(sample):
+def get_adj(array, sample):
     samples = []
     movex = [0]
     movey = [0]
@@ -88,7 +99,7 @@ def get_adj(sample):
     # gets samples from available grid squares
     for i in movex:
         for j in movey:
-            samples.append(get_samples(sample.x + i, sample.y + j))
+            samples.append(get_samples(array, sample.x + i, sample.y + j))
     return samples
 
 # A* search processing
@@ -96,45 +107,58 @@ def process(cSpace, start, dest):
     array = [[[] for x in range(AStar.width / AStar.grid)] for y in range(AStar.width / AStar.grid)]
     # map samples to their positions in grid space via their centroid positions
     for i in cSpace:
-        cx, cy, angle = centroid_angle(i)
-        array[y][x].append(Sample(i, cx, cy, cx / AStar.grid, cy / AStar.grid, angle))
+#         print i
+#         cx, cy, angle = centroid_angle(i)
+        cx, cy = centroid_angle(i)
+        array[cy / AStar.grid][cx / AStar.grid].append(Sample(i, cx, cy, cx / AStar.grid, cy / AStar.grid))  # , angle))
     
     # add end position to samples
-    cx, cy, angle = centroid_angle(dest)
-    AStar.end = Sample(dest, cx, cy, cx / AStar.grid, cy / AStar.grid, angle)
+#     cx, cy, angle = centroid_angle(dest)
+    cx, cy = centroid_angle(dest)
+    AStar.end = Sample(dest, cx, cy, cx / AStar.grid, cy / AStar.grid)  # , angle)
     array[cy / AStar.grid][cx / AStar.grid].append(AStar.end)
     
     # create start position sample
-    cx, cy, angle = centroid_angle(start)
-    AStar.start = Sample(start, cx, cy, cx / AStar.grid, cy / AStar.grid, angle)
+#     cx, cy, angle = centroid_angle(start)
+    cx, cy = centroid_angle(start)
+    AStar.start = Sample(start, cx, cy, cx / AStar.grid, cy / AStar.grid)  # , angle)
     AStar.start.h = get_h(AStar.start)
     AStar.start.f = AStar.start.g + AStar.start.f
     
     # start A* search
     heapq.heappush(AStar.op, (AStar.start.f, AStar.start))
     while len(AStar.op):
+#         print 'searching'
         # get sample from the open list based on samples f value
         f, sample = heapq.heappop(AStar.op)
         AStar.cl.add(sample)
+#         print sample.coords
+#         print AStar.end.coords
+        for i in range(len(sample.coords)):
+            if sample.coords[i] != AStar.end.coords[i]:
+                break
+            elif i == len(sample.coords) - 1:
+                return sample
         # check if it's the final position
-        if sample.coords is AStar.end.coords:
-            return sample
+#         if sample.coords == AStar.end.coords:
+#             return sample
         # get samples which are near to the current
-        adj_samples = get_adj(sample)
+        adj_samples = get_adj(array, sample)
         for c in adj_samples:
-            if c not in AStar.cl:
-                if (c.f, c) in AStar.op:
-                    if c.g > (sample.g + get_dist(sample, c)):
-                        update_sample(c, sample)
-                else:
-                    update_sample(c, sample)
-                    heapq.heappush(AStar.op, (c.f, c))
+            for d in c:
+                if d not in AStar.cl:
+                    if (d.f, d) in AStar.op:
+                        if d.g > (sample.g + get_dist(sample, d)):
+                            update_sample(d, sample)
+                    else:
+                        update_sample(d, sample)
+                        heapq.heappush(AStar.op, (d.f, d))
+    return False            
+        
 
 def main(inputfile, outputfile):
     global minArea
     global AStar
-    global obstacles
-    global array
     obstacles = []
     cSpace = []
     AStar = Astar()
@@ -144,17 +168,15 @@ def main(inputfile, outputfile):
     lines = file.readlines()
     start = remove_decimal(lines[1].strip('\n').split(' '))
     finish = remove_decimal(lines[2].strip('\n').split(' '))
+
     asv = []
     number = int(lines[0].strip('\n'))
-    rmin = 7 * (number - 1)
-    minArea = np.pi * rmin ** 2
-#     for i in range(number):
-#         # Create and initialise ASVs
-#         asv.append(ASV(i, start[i * 2], start[i * 2 + 1], finish[i * 2], finish[i * 2 + 1]))
-#         if i is not 0:
-#             asv[i].boom = start[-(number - i)]
-    # Extracts how much the obstacles are required to be expanded by 
-    expand = obstacle_transform(asv)
+    for i in range(number):
+        # Create and initialise ASVs
+        asv.append(ASV(i, start[i * 2], start[i * 2 + 1], finish[i * 2], finish[i * 2 + 1]))
+        if i is not 0:
+            asv[i].boom = start[-(number - i)]
+    asv[0].direction = ccw(asv[0], asv[1], asv[2])
     
 #    src = cv2.cv.fromarray(grid)
 #    cv2.cv.ShowImage('win1', src)
@@ -164,16 +186,23 @@ def main(inputfile, outputfile):
     for j in range(int(lines[3].strip('\n'))):
         obstacle = remove_decimal(lines[j + 4].strip('\n').split(' '))
         obstacles.append(obstacle)
-        final_obstacle = [obstacle[1] + expand[0], obstacle[5] + expand[1] + 1, obstacle[0] + expand[2], obstacle[4] + expand[3] + 1]
+        final_obstacle = [obstacle[1], obstacle[5] + 1, obstacle[0], obstacle[4] + 1]
         index = 0
         while index < len(final_obstacle):
             if final_obstacle[index] < 0:
                 final_obstacle[index] = 0
             index += 1 
         grid[final_obstacle[0]:final_obstacle[1], final_obstacle[2]:final_obstacle[3]] = 1
-
-    process(cSpace)
-    display_path(output)
+    
+    i = 1
+    cSpace = obtain_random_points(asv, 100000, obstacles, grid)
+    end = process(cSpace, start[:-number + 1], finish[:-number + 1])
+    while end == False:
+        print 'trying again'
+        cSpace = obtain_random_points(asv, 5000 * i * 5, obstacles, grid)
+        end = process(cSpace, start[:-number + 1], finish[:-number + 1])
+        i += 1
+    display_path(output, end, obstacles)
     output.close()
     file.close()
 
